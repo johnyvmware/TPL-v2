49216c412c124719a419c2245753912b037f8502
johnyvmware <123988613+johnyvmware@users.noreply.github.com>
2025-08-19 21:28:16 +0200

    Update CategoryAssignment model to make SubCategory nullable

--- src/TransactionProcessingSystem/Models/CategoryAssignment.cs
+++ src/TransactionProcessingSystem/Models/CategoryAssignment.cs
@@ -5,7 +5,7 @@ public record CategoryAssignment
 {
     public required string Reasoning { get; init; }
     public required string MainCategory { get; init; }
-    public required string SubCategory { get; init; }
+    public required string? SubCategory { get; init; }
 }
 
 
